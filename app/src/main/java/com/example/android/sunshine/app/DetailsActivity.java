1be6a7a50205a726b9bd1d8183ac389ffa83f0d6
sengrou <rou_tey92@hotmail.com>
2014-12-06 00:30:40 +0800

    display image

--- app/src/main/java/com/example/android/sunshine/app/DetailsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailsActivity.java
@@ -1,11 +1,8 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,15 +11,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.apache.http.util.ByteArrayBuffer;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 
 public class DetailsActivity extends ActionBarActivity {
@@ -70,13 +72,55 @@ public class DetailsActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
+            final String LOG_TAG = DetailsActivity.class.getSimpleName();
             Intent intent = getActivity().getIntent();
             View rootView = inflater.inflate(R.layout.fragment_details, container, false);
             if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
                 String birdStr = intent.getStringExtra(Intent.EXTRA_TEXT);
-                ((TextView)rootView.findViewById(R.id.detail_text)).setText(birdStr);
+                String position = intent.getStringExtra(Intent.EXTRA_TITLE);
+                Log.v(LOG_TAG, birdStr+"...position="+position);
+                //((TextView)rootView.findViewById(R.id.detail_text)).setText(birdStr);
 
-             /*   String IMAGE_URL= birdStr;
+
+              //  ImageView image;
+              //  image = (ImageView)findViewById(R.id.bird_image);
+
+             /*   File imgFile = new  File("/sdcard/Android/data/com.example.android.sunshine.app/bird.jpg");
+
+                if(imgFile.exists()){
+
+                    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+                    ImageView myImage = (ImageView) findViewById(R.id.bird_image);
+
+                    myImage.setImageBitmap(myBitmap);
+
+                }
+                Bitmap bmImg = BitmapFactory.decodeFile("path of your img1");
+                imageView.setImageBitmap(bmImg);
+*/
+                File imgFile = new  File("/sdcard/Android/data/com.example.android.sunshine.app/bird"+position+".jpg");
+                if(imgFile.exists())
+                {
+                    Log.v(LOG_TAG, "imgFile exist!");
+                    ImageView myImage = (ImageView) rootView.findViewById(R.id.bird_image);
+                    myImage.setImageURI(Uri.fromFile(imgFile));
+
+                }else{
+                    Log.v(LOG_TAG, "imgFile not downloaded yet!");
+                    ImageManager imageManager = new ImageManager();
+                    imageManager.DownloadFromUrl(birdStr, "bird"+position+".jpg");
+                    if(imgFile.exists())
+                    {
+                        Log.v(LOG_TAG, "imgFile not exist!");
+                        ImageView myImage = (ImageView) rootView.findViewById(R.id.bird_image);
+                        myImage.setImageURI(Uri.fromFile(imgFile));
+
+                    }
+                }
+
+
+                 /*   String IMAGE_URL= birdStr;
                 Uri builtUri = Uri.parse(IMAGE_URL).buildUpon().build();
 
                 // Create the request to OpenWeatherMap, and open the connection
@@ -108,4 +152,56 @@ public class DetailsActivity extends ActionBarActivity {
             return rootView;
         }
     }
+
+    public static class ImageManager {
+        private final String LOG_TAG = ImageManager.class.getSimpleName();
+        private final String PATH = "/sdcard/Android/data/com.example.android.sunshine.app/";
+       // File root = Environment.getExternalStorageDirectory();
+
+        public void DownloadFromUrl(String imageURL, String fileName) {  //this is the downloader method
+            try {
+                URL url = new URL(imageURL); //you can write here any link
+
+              //  File file = new File(root, fileName);
+              //  file.createNewFile();
+                File sunshineDirectory = new File("/sdcard/Android/data/com.example.android.sunshine.app/");
+                // have the object build the directory structure, if needed.
+                sunshineDirectory.mkdirs();
+                File file = new File(sunshineDirectory, fileName);
+                long startTime = System.currentTimeMillis();
+                Log.d(LOG_TAG, "download begining");
+                Log.d(LOG_TAG, "download url:" + url);
+                Log.d(LOG_TAG, "downloaded file name:" + fileName);
+                        /* Open a connection to that URL. */
+                URLConnection ucon = url.openConnection();
+
+                        /*
+                         * Define InputStreams to read from the URLConnection.
+                         */
+                InputStream is = ucon.getInputStream();
+                BufferedInputStream bis = new BufferedInputStream(is);
+
+                        /*
+                         * Read bytes to the Buffer until there is nothing more to read(-1).
+                         */
+                ByteArrayBuffer baf = new ByteArrayBuffer(50);
+                int current = 0;
+                while ((current = bis.read()) != -1) {
+                    baf.append((byte) current);
+                }
+                        /* Convert the Bytes read to a String. */
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.write(baf.toByteArray());
+                fos.close();
+                Log.d(LOG_TAG, "download ready in"
+                        + ((System.currentTimeMillis() - startTime) / 1000)
+                        + " sec");
+
+            } catch (IOException e) {
+                Log.d(LOG_TAG, "Error: " + e);
+            }
+
+        }
+    }
+
 }
